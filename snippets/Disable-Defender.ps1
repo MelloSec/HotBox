99cbb5a62138eed4547e54d919b779097cd16c31
mellonaut <65114647+mellonaut@users.noreply.github.com>
2022-07-03 08:40:50 -0400

    Defender Disable
    Found these in DetectionLab, they do it different than mine and I'm pretty sure  theirs is better. It has the vagrant paths still, just tidy it up and make it so it selects whatever the current user profile is so you can run it wherever you want and not just in thee labs

---
+++ snippets/Disable-Defender.ps1
@@ -0,0 +1,35 @@
+$hostname = $(hostname)
+
+# Disabling the progress bar speeds up IWR https://github.com/PowerShell/PowerShell/issues/2138
+$ProgressPreference = 'SilentlyContinue'
+# GitHub requires TLS 1.2 as of 2/27
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+# Windows Defender should be disabled already by the GPO
+# Change this to jus grab current users profile to exlcude 
+If ($hostname -eq "win10" -And (Get-Service -Name WinDefend).StartType -ne 'Disabled' ) {
+  # Adding Defender exclusions just in case
+  Set-MpPreference -ExclusionPath "C:\Tools"
+  Add-MpPreference -ExclusionPath "C:\Users\vagrant\AppData\Local\Temp"
+
+  . c:\vagrant\scripts\Invoke-CommandAs.ps1
+  Invoke-CommandAs 'NT SERVICE\TrustedInstaller' {
+    Set-Service WinDefend -StartupType Disabled
+    Stop-Service WinDefend
+  }
+}
+
+# Windows Defender should be disabled by the GPO or uninstalled already, but we'll keep this just in case
+If ($hostname -ne "win10" -And (Get-Service -Name WinDefend -ErrorAction SilentlyContinue).status -eq 'Running') {
+  # Uninstalling Windows Defender (https://github.com/StefanScherer/packer-windows/issues/201)
+    Write-Host "$('[{0:HH:mm}]' -f (Get-Date)) Uninstalling Windows Defender..."
+  Try {
+    Uninstall-WindowsFeature Windows-Defender -ErrorAction Stop
+    Uninstall-WindowsFeature Windows-Defender-Features -ErrorAction Stop
+  } Catch {
+    Write-Host "$('[{0:HH:mm}]' -f (Get-Date)) Windows Defender did not uninstall successfully..."
+    Write-Host "$('[{0:HH:mm}]' -f (Get-Date)) We'll try again during install-red-team.ps1"
+  }
+} Else  {
+  Write-Host "$('[{0:HH:mm}]' -f (Get-Date)) Windows Defender has already been disabled or uninstalled."
+}
